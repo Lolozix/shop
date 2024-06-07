7b616e0b03d14e16a049adbaa3e325f0e51ab792
Jujuba <maronez.juliana@gmail.com>
2024-06-07 02:55:10 -0300

    Input inserido

--- src/app/components/Cabecalho.js
+++ src/app/components/Cabecalho.js
@@ -27,7 +27,7 @@ export default function Cabecalho(){
           </Link>
 
        </ul>
-
+      <input className={styles.input}></input>
        <button className={styles.botao}>Adicionar um Novo Produto</button>
        </nav>
       </header>
